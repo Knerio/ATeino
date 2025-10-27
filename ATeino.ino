cd5c7e0cc52bee793422c4f02f53cde7def5db91
dario.pranjic <dario.pranjic@EULT-PF4319TK-1.adv.advantest.com>
2025-10-27 11:17:18 +0100

    Controllable LED via button

--- ATeino.ino
+++ ATeino.ino
@@ -1,7 +1,13 @@
 void setup() {
   Serial.begin(9600);
+  pinMode(13, OUTPUT);
+  pinMode(2, INPUT_PULLUP);
 }
 
 void loop() {
-  Serial.println("Hello World");
+  if (digitalRead(2) == HIGH) {
+    digitalWrite(13, LOW);
+  } else {
+    digitalWrite(13, HIGH);
+  }
 }
