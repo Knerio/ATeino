9db29270da49f35703ee27dcb5f6e269c82e880c
dario.pranjic <dario.pranjic@EULT-PF4319TK-1.adv.advantest.com>
2025-10-27 13:23:34 +0100

    First testing concept

--- ATeino.ino
+++ ATeino.ino
@@ -1,12 +1,56 @@
+const uint8_t pin_map[][3] = {
+  {9, A1, A2},
+  {A3, A4, A5},
+  {7, 6, 5},
+  {4, 3, 2}
+};
+
+
 void setup() {
   Serial.begin(9600);
-  pinMode(9, OUTPUT);
-  pinMode(2, INPUT_PULLUP);
+  for(int i = 0; i < sizeof(pin_map) / sizeof(pin_map[0]); i++) {
+    pinMode(pin_map[i][0], OUTPUT);
+    pinMode(pin_map[i][1], OUTPUT);
+    pinMode(pin_map[i][2], INPUT);
+  }
+
+
+  Serial.println();
+  Serial.println("Testing chip: ");
+  
+  int successes = 0;
+  int fails = 0;
+
+  for(int i = 0; i < sizeof(pin_map) / sizeof(pin_map[0]); i++) {
+    if(test(pin_map[i][0], pin_map[i][1], pin_map[i][2])) {
+      successes++;
+    } else {
+      fails++;
+    }
+  }
+
+  Serial.println("Ran " + String(successes + fails) + " tests.");
+  Serial.println("Successes: " + String(successes));
+  Serial.println("Fails: " + String(fails));
+}
+
+bool test(uint8_t pin_1, uint8_t pin_2, uint8_t output_pin) {
+  digitalWrite(pin_1, HIGH);
+  digitalWrite(pin_2, HIGH);
+  if (digitalRead(output_pin) != 1) return false;
+  digitalWrite(pin_1, HIGH);
+  digitalWrite(pin_2, LOW);
+  if (digitalRead(output_pin) != 0) return false;
+  digitalWrite(pin_1, LOW);
+  digitalWrite(pin_2, HIGH);
+  if (digitalRead(output_pin) != 0) return false;
+  digitalWrite(pin_1, LOW);
+  digitalWrite(pin_2, LOW);
+  if (digitalRead(output_pin) != 0) return false;
+
+  return true;
 }
 
 void loop() {
-  int brightness = analogRead(A5) * 0.25; 
-  if (brightness < 50) brightness = 0;
-  analogWrite(9, brightness);
-  Serial.println(brightness);
+
 }
